671a90bf5f66dbd5ef44f663c40da2855fae9e68
sabyasachi das chowdhury <sabyasachidaschowdhury1@gmail.com>
2025-12-23 10:06:38 +0600

    Update LCS.cpp

--- LCS.cpp
+++ LCS.cpp
@@ -1,34 +1,57 @@
 #include<iostream>
-#include<cstring>
+#include<string>
+#include<algorithm>
 using namespace std;
-int main (){
-    char x[100] , y[100];
-    cout<<"Enter first string :";
-    cin>>x;
-    cout<<"Enter second string :";
-    cin>>y;
-    int m=strlen(x);
-    int n=strlen(y);
+string printlcs(string x , string y , int &length){
+    int m=x.length();
+    int n=y.length();
     
-   int dp[101][101];
-    
-    for(int i=0 ; i<=m;i++){
-        for(int j=0;j<=n;j++){
-            if( i ==0 || j==0){
-                dp[i][j]=0;
-            }
+    int dp[100][100];
+    for(int i =0;i<m;i++){
+        for(int j =0;j<n;j++){
+            dp[i][j]=0;
         }
     }
     
-    for(int i= 1;i<=m;i++){
+    for(int i=1;i<=m;i++){
         for(int j=1;j<=n;j++){
             if(x[i-1]==y[j-1]){
-                dp[i][j]=dp[i-1][j-1]+1;
+                dp[i][j]=1+dp[i-1][j-1];
             }
             else{
                 dp[i][j]=max(dp[i-1][j] , dp[i][j-1]);
             }
         }
     }
-    cout<<"The minimum length :"<<dp[m][n];
+    length=dp[m][n];
+    int i=m , j=n;
+    string lcs="";
+    while(i>0 && j>0){
+        if(x[i-1] == y[j-1]){
+            lcs=x[i-1]+lcs;
+            i--;
+            j--;
+        }else{
+            if(dp[i-1][j] > dp[i][j-1])
+            i--;
+            else
+            j--;
+        }
+        
+    }
+    return lcs;
+}
+int main(){
+    string str1 , str2;
+    cout<<"Enter first string :";
+    cin>>str1;
+    cout<<"Enter second strinf :";
+    cin>>str2;
+    
+    int lcslength;
+    
+    string lcs=printlcs(str1 , str2 ,lcslength);
+    cout<<"LCS charater"<<lcs;
+    cout<<"Length :"<<lcslength;
+    
 }
